1888d8572b54e4b82688ead4cceda05bf3aac4d1
berenif <florian.berenice@gmail.com>
2020-06-09 17:29:16 +0200

    Getting started with EFCORE

---
+++ src/ConsoleApp/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Linq;
+using SamuraiApp.Data;
+using SamuraiApp.Domain;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        private static SamuraiContext context = new SamuraiContext();
+
+        static void Main(string[] args)
+        {
+
+            context.Database.EnsureDeleted();
+
+            context.Database.EnsureCreated();
+
+            Console.WriteLine("Hello World!");
+
+            GetSamurais();
+            AddSamurai();
+            GetSamurais();
+        }
+
+        static void AddSamurai()
+        {
+            var samurai = new Samurai { Name = "Jean" };
+
+            context.Add(samurai);
+            context.SaveChanges();
+        }
+
+        static void GetSamurais()
+        {
+            var samurais = context.Samurais.ToList();
+
+            System.Console.WriteLine($"There is currently {samurais.Count}");
+
+            foreach (var item in samurais)
+            {
+                System.Console.WriteLine(item.Name);
+            }
+        }
+    }
+}
---
+++ src/SamuraiApp.Domain/Quote.cs
@@ -0,0 +1,15 @@
+using System.Collections.Generic;
+
+namespace SamuraiApp.Domain
+{
+    public class Quote
+    {
+        public int Id { get; set; }
+
+        public string Text { get; set; }
+
+        public Samurai Samurai { get; set; }
+
+        public int SamuraiId { get; set; }
+    }
+}
